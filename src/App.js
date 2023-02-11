0919f1cfba47cb69f7e2878759af38b90b9c549e
ThinhNguyen <thinhnguyen042002@gmail.com>
2023-02-12 00:42:23 +1100

    Material design deployement testing

--- src/App.js
+++ src/App.js
@@ -1,115 +1,118 @@
-import React, {useState, useRef} from 'react';
+import React from 'react';
+//{useState, useRef}
 import './App.css';
 
-import firebase from 'firebase/compat/app';
+// import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-import {useAuthState} from 'react-firebase-hooks/auth';
-import {useCollectionData} from 'react-firebase-hooks/firestore'
+// import {useAuthState} from 'react-firebase-hooks/auth';
+// import {useCollectionData} from 'react-firebase-hooks/firestore'
+import Register from './pages/Login/Register';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBwW0Bycz6-VHfbNErartsPpBmuqZDoxQE",
-  authDomain: "react-web-app-b2406.firebaseapp.com",
-  projectId: "react-web-app-b2406",
-  storageBucket: "react-web-app-b2406.appspot.com",
-  messagingSenderId: "56227368269",
-  appId: "1:56227368269:web:82152030def8bed29e7b04"
-})
+// firebase.initializeApp({
+//   apiKey: "AIzaSyBwW0Bycz6-VHfbNErartsPpBmuqZDoxQE",
+//   authDomain: "react-web-app-b2406.firebaseapp.com",
+//   projectId: "react-web-app-b2406",
+//   storageBucket: "react-web-app-b2406.appspot.com",
+//   messagingSenderId: "56227368269",
+//   appId: "1:56227368269:web:82152030def8bed29e7b04"
+// })
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
 
 function App() {
 
-  const [user] = useAuthState(auth);
+  // const [user] = useAuthState(auth);
 
   return (
-    <div className="App">
-      <header>
-        <SignOut/>
+    // <div className="App">
+    //   <header>
+    //     <SignOut/>
 
-      </header>
+    //   </header>
 
-      <section>
-        {user ? <ChatRoom/> : <SignIn/>}
-      </section>
-    </div>
+    //   <section>
+    //     {user ? <ChatRoom/> : <SignIn/>}
+    //   </section>
+    // </div>
+    <Register/>
   );
 }
 
-function SignIn(){
-  const SignInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
+// function SignIn(){
+//   const SignInWithGoogle = () => {
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     auth.signInWithPopup(provider);
+//   }
 
-  return(
-    <button onClick={SignInWithGoogle}>Sign in with Google</button>
-  )
-}
+//   return(
+//     <button onClick={SignInWithGoogle}>Sign in with Google</button>
+//   )
+// }
 
-function SignOut(){
-  return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>  
-  )
-}
+// function SignOut(){
+//   return auth.currentUser && (
+//     <button onClick={() => auth.signOut()}>Sign Out</button>  
+//   )
+// }
 
-function ChatRoom() {
-  const dummy = useRef();
+// function ChatRoom() {
+//   const dummy = useRef();
 
-  const messageRef = firestore.collection('messages');
-  const query = messageRef.orderBy('createAt');
+//   const messageRef = firestore.collection('messages');
+//   const query = messageRef.orderBy('createAt');
 
-  const [messages] = useCollectionData(query, {idField: 'id'});
+//   const [messages] = useCollectionData(query, {idField: 'id'});
 
-  const [formValue, setFormValue] = useState('');
+//   const [formValue, setFormValue] = useState('');
 
-  const sendMessage = async(e) => {
-    e.preventDefault();
-    const {uid, photoURL} = auth.currentUser;
+//   const sendMessage = async(e) => {
+//     e.preventDefault();
+//     const {uid, photoURL} = auth.currentUser;
 
-    await messageRef.add({
-      text: formValue,
-      createAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL    
-    })
+//     await messageRef.add({
+//       text: formValue,
+//       createAt: firebase.firestore.FieldValue.serverTimestamp(),
+//       uid,
+//       photoURL    
+//     })
 
-    setFormValue('');
+//     setFormValue('');
 
-    dummy.current.scrollIntoView({behavior: 'smooth'});
+//     dummy.current.scrollIntoView({behavior: 'smooth'});
 
-  }
+//   }
 
 
-  return (
-    <>
-      <main>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
+//   return (
+//     <>
+//       <main>
+//         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
 
-        <div ref={dummy}></div>
+//         <div ref={dummy}></div>
         
-      </main>
-      <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
-        <button type="submit">Send</button>
-      </form>
-    </>
-  )
-}
+//       </main>
+//       <form onSubmit={sendMessage}>
+//         <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+//         <button type="submit">Send</button>
+//       </form>
+//     </>
+//   )
+// }
 
-function ChatMessage(props){
-  const {text, uid, photoURL} = props.message;
+// function ChatMessage(props){
+//   const {text, uid, photoURL} = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+//   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-  return (
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL} alt="..."/>
-      <p>{text}</p>
-    </div>
-  )
-}
+//   return (
+//     <div className={`message ${messageClass}`}>
+//       <img src={photoURL} alt="..."/>
+//       <p>{text}</p>
+//     </div>
+//   )
+// }
 
 export default App;
